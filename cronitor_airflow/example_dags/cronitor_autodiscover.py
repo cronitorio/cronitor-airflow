a3a48279dc6d68ddb563f0d56583aaa7d1d52d34
jj <jj@jdotjdot.com>
2022-04-21 23:01:45 -0400

    Add autoprovision DAG for monitors

--- cronitor_airflow/example_dags/cronitor_autodiscover.py
+++ cronitor_airflow/example_dags/cronitor_autodiscover.py
@@ -2,14 +2,15 @@ from datetime import timedelta
 from datetime import datetime
 from airflow.cli import cli_parser
 from airflow.configuration import conf
-from airflow.models import DagBag, DAG
+from airflow.models import DagBag, DAG, DagRun
 from airflow.decorators import dag, task
 import argcomplete
-import croniter
 import cronitor
+from airflow.operators.python import get_current_context
 import os
 import logging
 from cronitor_airflow.hooks.cronitor_hook import CronitorHook
+from airflow.timetables.interval import CronDataIntervalTimetable, DeltaDataIntervalTimetable
 
 
 def do_parsing(input_args):
@@ -39,7 +40,7 @@ def convert_dag_to_monitor(dag: DAG) -> dict:
     start_date=datetime(2022, 1, 1),
     tags=['cronitor'],
 )
-def autocollect_dags(cronitor_conn_id='cronitor_default'):
+def autoprovision_monitors(cronitor_conn_id='cronitor_default'):
     @task()
     def get_dags():
         """Get all active, non-paused DAGs and convert them to Cronitor monitors"""
@@ -47,22 +48,29 @@ def autocollect_dags(cronitor_conn_id='cronitor_default'):
         outlist = []
         for dag in dags.values():
             if dag.get_is_active() and not dag.get_is_paused():
-                # TODO: This doesn't work yet, this isn't the right way to check if
-                # the schedule is a weird timetable or a regular cron expression
-                if croniter.croniter.is_valid(dag.timetable.summary):
+                if isinstance(dag.timetable, (CronDataIntervalTimetable, DeltaDataIntervalTimetable)):
                     outlist.append(convert_dag_to_monitor(dag))
                 else:
                     logging.warning(f"DAG '{dag.dag_id}' does not use a cron-based schedule or has an invalid schedule "
-                                    f"or timetable: '{dag.timetable.summary}'. Cronitor autodiscover currently only supports cron-based "
-                                    f"schedules/timetables. Skipping the DAG.")
+                                    f"or timetable: '{type(dag.timetable)}'. Cronitor autodiscover currently only supports "
+                                    f"cron- or interval-based schedules/timetables. Skipping the DAG.")
                     continue
         return outlist
 
     @task()
     def put_monitors(monitors):
-        CronitorHook().get_connection(cronitor_conn_id)
-        cronitor.api_key = ''
+        # Can get context this way rather than having it pass in via args
+        # https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html#accessing-context-variables-in-decorated-tasks
+        context = get_current_context()
+        connection = CronitorHook().get_connection(context['params']['cronitor_conn_id'])
+        cronitor.api_key = connection.password
         cronitor.Monitor.put(monitors)
+
+    @task()
+    def get_dagruns_for_monitors(monitors):
+        pass
+        ## Find out the last time this dag was run, and then for every other DAG,
+        ## send all of the telemetry since the last run to Cronitor
 
     monitors = get_dags()
     put_monitors(monitors)
